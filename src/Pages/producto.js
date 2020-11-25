d4ca1064e96a01a5e899355ff44fc25e4654664b
Felipe Palacio <66035015+FelipePala0724@users.noreply.github.com>
2020-11-25 12:01:53 -0500

    Nuevo Cambio Vercel

--- src/Pages/producto.js
+++ src/Pages/producto.js
@@ -3,7 +3,13 @@ import LocionPacoRabanne from "../Imagenes/375x500.46038.jpg";
 import "../Style/style.css";
 
 class Producto extends React.Component {
+render(
 
+){
+return(
+  <div></div>
+)
+}
 
 }
 
